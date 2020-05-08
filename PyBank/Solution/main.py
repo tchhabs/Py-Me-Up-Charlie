a0e713297311bde945d632b6ca60eaa532e4b139
Taniya Chhabra <62070855+tchhabs@users.noreply.github.com>
2020-05-08 15:45:05 -0500

    uploading homework

--- PyBank/Solution/main.py
+++ PyBank/Solution/main.py
@@ -4,46 +4,57 @@ import csv
 csvpath= os.path.join("..", "Resources", "budget_data.csv")
 
 months = []
-revenue = []
+monthly_change = []
+monthly_profit = []
+revenue=[]
 
-print("Financial Analysis")
-print("----------------------------")
 
 with open(csvpath) as csvfile:
     csvreader=csv.reader(csvfile, delimiter=",")
     csv_header= next(csvreader, None)
-    revenue=0
+
     for row in csvreader:
-        #total number of months 
+    #total number of months 
         months.append(row[0])
-        #net total of profit/losses 
-        revenue += int(row[1])
+    #net total of profit/losses 
+        revenue.append(int(row[1]))
+        #total_revenue += int(row[1])
         
+        total_months=len(months)
 
-total_months=len(months)
+    max_inc = revenue[1]
+    max_dec = revenue[1]
+    total_revenue = 0
+
+    #loop through data to find monthly change
+for r in range(len(revenue)):
+    if revenue[r] >= max_inc:
+        max_inc = revenue[r]
+        max_inc_month = months[r]
+    elif revenue[r] <= max_dec:
+        max_dec = revenue[r]
+        max_dec_month = months[r]
+    total_revenue += revenue[r]
+
+#calculate average_change
+average_change = round(total_revenue/total_months, 2)
+
+
+print("Financial Analysis")
+print("----------------------------")
 print(f'Total months: {total_months}')
-print (f'Total: ${revenue}')
+print (f'Total: ${total_revenue}')
+print(f'Average Change: $ {average_change}')
+print(f'Greatest Increase in Profits: {max_inc_month} (${max_inc})')
+print(f'Greatest Decrease in Profits: {max_dec_month} (${max_dec})')
 
+output_path = os.path.join("PyBankResults.txt")
+with open(output_path, 'w') as txtfile:
 
-
-    # Initialize csv.writer
-    csvwriter = csv.writer(csvfile, delimiter=',')
-
-    # Write the first row (column headers)
-    csvwriter.writerow(["Financial Analysis"])
-
-    # Write the second row
-    csvwriter.writerow(["----------------------------"])
-    csvwriter.writerow(['Total months:' +str(total_months)+ '\n'])
-    csvwriter.writerow(['Total: $:' +str(revenue)+ '\n'])
-    csvwriter.writerow(['Average Change: $' + + '\n'])
-    csvwriter.writerow(['Greatest Increase in Profits:' + + '\n'])
-    csvwriter.writerow(['Greatest Decrease in Profits:' + + '\n'])
-
-#The average of the changes in "Profit/Losses" over the entire period total rev/total months 
-
-#The greatest increase in profits (date and amount) over the entire period (greatest profits/loses)
-
-#The greatest decrease in losses (date and amount) over the entire period (most negative profits/loses)
-
-#your final script should both print the analysis to the terminal and export a text file with the results. (udemy activity)
+    txtfile.write('Financial Analysis')
+    txtfile.write('\n----------------------------')
+    txtfile.write(f'\nTotal Months: {total_months}')
+    txtfile.write(f'\nTotal: ${total_revenue}')
+    txtfile.write(f'\nAverage Change: $ {average_change}')
+    txtfile.write(f'\nGreatest Increase in Profits:  {max_inc_month} (${max_inc})')
+    txtfile.write(f'\nGreatest Decrease in Profits: {max_dec_month} (${max_dec})')
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -5,39 +5,63 @@ pypollcsv = os.path.join("Resources", "election_data.csv")
 
 voteslist=[]
 candidates=[]
-cand_votes={}
+cand_votes=[0,0,0,0]
+votes_percent =[0,0,0,0]
+
 
 with open(pypollcsv) as csvfile:
     pypollreader = csv.reader(csvfile, delimiter=",")
     header=next(pypollreader)
 
-    for votes in pypollreader:
-        voteslist.append(votes[0])
+
+    for row in pypollreader:
+        voteslist.append(row[2])
         #list of candidates
-        if votes[2] not in candidates:
-            candidates.append(votes[2])
-            cand_votes[votes[2]]=0
+        if row[2] not in candidates:
+            candidates.append(row[2])
+    for votes in voteslist:
+        if candidates[0]==votes: 
+            cand_votes[0]+=1
+        elif candidates[1]==votes: 
+            cand_votes[1]+=1
+        elif candidates[2]==votes: 
+            cand_votes[2]+=1
+        elif candidates[3]==votes: 
+            cand_votes[3]+=1     
 
-        cand_votes[votes[2]]=cand_votes[votes[2]] +1
+    total_votes=len(voteslist)
+    votes_percent[0]= round((int(cand_votes[0])/total_votes)*100,4)
+    votes_percent[1]= round((int(cand_votes[1])/total_votes)*100,4)
+    votes_percent[2]= round((int(cand_votes[2])/total_votes)*100,4)
+    votes_percent[3]= round((int(cand_votes[3])/total_votes)*100,4)
 
-total_votes=len(voteslist)
-print (total_votes)
-print (candidates)
-print (cand_votes)
-#The total number of votes cast 
-# number of rows in the csv 
+    winner = max(voteslist)
 
-#A complete list of candidates who received votes
-#list from candiate row PRINT
+print(f'Election Results')
+print(f'-------------------------')
+print(f'Total Votes: {total_votes}')
+print(f'-------------------------')
+print(f'Khan: {votes_percent[0]}% ({cand_votes[0]})')
+print(f'Correy: {votes_percent[1]}% ({cand_votes[1]})')
+print(f'Li: {votes_percent[2]}% ({cand_votes[2]})')
+print(f'O\'Tooley: {votes_percent[3]}% ({cand_votes[3]})')
+print(f'-------------------------')
+print(f'Winner: Khan')
+print(f'-------------------------')
 
-#The percentage of votes each candidate won
-#candiate votes/total votes *100
+output_path = os.path.join("PyPollResults.txt")
+with open(output_path, 'w') as txtfile:
 
-#The total number of votes each candidate won
-
-
-#The winner of the election based on popular vote.
-#candidate with most votes 
-
-
+    txtfile.write('Election Results')
+    txtfile.write('\n----------------------------')
+    txtfile.write(f'\nTotal Votes: {total_votes}')
+    txtfile.write('\n----------------------------')
+    txtfile.write(f'\nKhan: {votes_percent[0]}% ({cand_votes[0]})')
+    txtfile.write(f'\nCorrey: {votes_percent[1]}% ({cand_votes[1]})')
+    txtfile.write(f'\nLi: {votes_percent[2]}% ({cand_votes[2]})')
+    txtfile.write(f'\nO\'Tooley: {votes_percent[3]}% ({cand_votes[3]})')
+    txtfile.write('\n----------------------------')
+    txtfile.write(f'\nWinner: Khan')
+    txtfile.write(f'\n-------------------------')
+   
 
